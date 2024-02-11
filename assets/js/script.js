fa6f7461177826485a685c07951ac7b3c5f3a97a
Seraphin Sk <seraphinsk03@gmail.com>
2024-02-11 11:09:09 +0100

    add section choosus

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,22 @@ const blurHeader = () => {
                        : header.classList.remove('blur-header')
 };
 window.addEventListener('scroll', blurHeader)
+
+
+
+/*=============== COMPTER ===============*/
+
+let number = 999; // Nombre final du compteur
+let compter = 0; // Initialisation du compteur
+let duration = 1; // Durée en seconde pendant laquel le compteur ira de 0 à 9999
+let delta = Math.ceil((duration * 1000) / number); // On calcule l'intervalle de temps entre chaque rafraîchissement du compteur (durée mise en milliseconde)
+let node =  document.getElementById("choosus__number"); // On récupère notre noeud où sera rafraîchi la valeur du compteur
+ 
+function countdown() {
+  node.innerHTML = ++compter + " + " ;
+  if( compter < number ) { // Si on est pas arrivé à la valeur finale, on relance notre compteur une nouvelle fois
+     setTimeout(countdown, delta);
+  }
+}
+ 
+setTimeout(countdown, delta);
